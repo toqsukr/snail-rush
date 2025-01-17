d0ddcb7677560ea5667f5ac00af263f84bc2096d
Wastard <Ilya.pakylov@gmail.com>
2025-01-17 19:06:30 +0300

    changed host url

--- src/modules/app/constant.ts
+++ src/modules/app/constant.ts
@@ -1,3 +1,3 @@
-export const HTTP_HOST_URL = 'http://213.171.31.233:10000'
+export const HTTP_HOST_URL = 'http://localhost:10000'
 // '213.171.31.233:10000'
-export const WS_HOST_URL = 'ws://213.171.31.233:10000'
+export const WS_HOST_URL = 'ws://localhost:10000'
