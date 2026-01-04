fd37c7cf28026d34a62e4378da42e1e478ee033a
wastard <Ilya.pakylov@gmail.com>
2026-01-04 19:47:51 +0300

    fix(lobby-menu): double ready bug

--- src/pages/home/ui/lobby-menu.tsx
+++ src/pages/home/ui/lobby-menu.tsx
@@ -5,13 +5,11 @@ import {
   PauseButton,
   useLobbyMenuDeps,
   useMainMenuDeps,
-  usePlay,
 } from '@features/menu'
 import { Html } from '@react-three/drei'
 import { queryClient } from '@shared/api/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { FC, useEffect, useRef } from 'react'
-import { useGameStore } from '@features/game'
+import { FC } from 'react'
 
 const LOBBY_MENU_ROTATION = [0, 0, 0] satisfies [number, number, number]
 const LOBBY_MENU_POSITION = [16.1, 35, -5] satisfies [number, number, number]
@@ -19,16 +17,6 @@ const LOBBY_MENU_POSITION = [16.1, 35, -5] satisfies [number, number, number]
 const LobbyMenu: FC = () => {
   const mainMenuContextValue = useMainMenuDeps()
   const lobbyContextValue = useLobbyMenuDeps()
-  const started = useGameStore(s => s.started)
-  const prevStarted = useRef(started)
-  const { playAction } = usePlay()
-
-  useEffect(() => {
-    if (started && !prevStarted.current) {
-      playAction()
-    }
-    prevStarted.current = started
-  }, [started])
 
   return (
     <Html
